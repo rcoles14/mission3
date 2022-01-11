bb789b87286a51b2d39383ef02404bc00fc046ca
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2022-01-10 22:02:58 -0700

    Add project files.

---
+++ mission2/Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ mission2/wwwroot/js/calculator.js
@@ -0,0 +1,51 @@
+﻿$('#submit').click(function () {
+    var assignments = parseFloat($('#assignments').val());
+    var groupproj = parseFloat($('#GroupProj').val());
+    var quizes = parseFloat($('#quizes').val());
+    var exams = parseFloat($('#exams').val());
+    var intex = parseFloat($('#intex').val());
+    var finalgrade = parseFloat(assignments * (.55) + groupproj * (.05) + quizes * (.10) + exams * (.20) + intex * (.10)).toFixed(2);
+    var output = '';
+    if (finalgrade >= 94) {
+        output = ('Wow! You earned an A with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 90) {
+        output = ('Good Job! You earned an A- with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 87) {
+        output = ('Good Job! You earned an B+ with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 84) {
+        output = ('You earned an B with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 80) {
+        output = ('You earned an B- with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 77) {
+        output = ('You earned an C+ with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 74) {
+        output = ('You earned an C with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 70) {
+        output = ('You earned an C- with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 67) {
+        output = ('You earned an D+ with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 64) {
+        output = ('You earned an D with a ' + finalgrade + "%")
+    }
+    else if (finalgrade >= 60) {
+        output = ('You earned an D- with a ' + finalgrade + "%")
+    }
+    else if (finalgrade < 60) {
+        output = ('You failed the course with a ' + finalgrade + "%")
+    }
+    if (output != '') {
+        $('#output').html(output);
+    }
+    else {
+        alert("Please correctly fill out the form")
+    }
+})
