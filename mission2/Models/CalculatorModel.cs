eda6f279785589b4af018fe8c391ba061bc17340
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2022-01-19 23:33:57 -0700

    validation

--- mission2/Models/CalculatorModel.cs
+++ mission2/Models/CalculatorModel.cs
@@ -11,19 +11,19 @@ namespace mission2.Models
     public class CalculatorModel
     {
         [Required]
-        
+        [Range(0, 100)]
         public float assignments { get; set; }
         [Required]
-        
+        [Range(0, 100)]
         public float GroupProj { get; set; }
         [Required]
-        
+        [Range(0, 100)]
         public float quizes { get; set; }
         [Required]
-        
+        [Range(0, 100)]
         public float exams { get; set; }
         [Required]
-     
+        [Range(0, 100)]
         public float intex { get; set; }
     }
 }
